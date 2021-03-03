a8cb6ca79d3c89787b55044b6326d0d925f614b1
renanmuniz <renan.mmerlo@gmail.com>
2021-03-02 22:57:42 -0300

    Criacao do projeto

---
+++ springbank/src/main/java/br/com/springbank/SpringbankApplication.java
@@ -0,0 +1,13 @@
+package br.com.springbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbankApplication.class, args);
+	}
+
+}
