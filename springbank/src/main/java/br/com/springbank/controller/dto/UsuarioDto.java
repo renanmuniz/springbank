5cebe036c298a631c56a3af9ad818ab743408a4a
renanmuniz <renan.mmerlo@gmail.com>
2021-03-05 00:42:49 -0300

    1) Otimizado as configuracoes de seguranca baseado em perfis. 2) Incluido perfil do usuario no detalhar usuario

--- springbank/src/main/java/br/com/springbank/controller/dto/UsuarioDto.java
+++ springbank/src/main/java/br/com/springbank/controller/dto/UsuarioDto.java
@@ -1,16 +1,22 @@
 package br.com.springbank.controller.dto;
 
+import br.com.springbank.modelo.Perfil;
 import br.com.springbank.modelo.Usuario;
 import org.springframework.data.domain.Page;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UsuarioDto {
 
     private Long id;
     private String nome;
+    private List<Perfil> perfis = new ArrayList<>();
 
     public UsuarioDto(Usuario usuario) {
         this.id = usuario.getId();
         this.nome = usuario.getNome();
+        this.perfis = usuario.getPerfis();
     }
 
     public static Page<UsuarioDto> converter(Page<Usuario> usuarios) {
@@ -31,5 +37,13 @@ public class UsuarioDto {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public List<Perfil> getPerfis() {
+        return perfis;
+    }
+
+    public void setPerfis(List<Perfil> perfis) {
+        this.perfis = perfis;
     }
 }
